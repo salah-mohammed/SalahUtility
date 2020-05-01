889dd174cc15201a5b571b3a60300724d3219128
Salah <Salahalimohamed1995@gmail.com>
2020-05-01 18:41:41 +0300

    no message

---
+++ SalahUtility/BaseExtension.swift
@@ -0,0 +1,15 @@
+//
+//  BaseExtension.swift
+//  Test1
+//
+//  Created by Salah on 12/15/19.
+//  Copyright © 2019 Salah. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+import AudioToolbox
+//import CommonCrypto
+/*    **Array**   */
+
