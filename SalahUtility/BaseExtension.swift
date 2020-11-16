8c9c57befe5bac93b7e88f1c877f2b0be47aaeb1
Salah <Salahalimohamed1995@gmail.com>
2020-11-16 13:37:58 +0200

    no message

--- SalahUtility/BaseExtension.swift
+++ SalahUtility/BaseExtension.swift
@@ -2322,3 +2322,10 @@ extension Array where Element == Dictionary<String, Any> {
         return nil;
     }
 }
+public var bs_hasTopNotch: Bool {
+   if #available(iOS 13.0,  *) {
+       return UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.safeAreaInsets.top ?? 0 > 20
+   }else{
+    return UIApplication.shared.delegate?.window??.safeAreaInsets.top ?? 0 > 20
+   }
+}
