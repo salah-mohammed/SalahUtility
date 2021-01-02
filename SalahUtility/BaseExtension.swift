5c74ca3127d71735cad36f1edc2f66c523c688f7
Salah <Salahalimohamed1995@gmail.com>
2021-01-02 19:18:11 +0200

    no message

--- SalahUtility/BaseExtension.swift
+++ SalahUtility/BaseExtension.swift
@@ -848,6 +848,14 @@ public extension NSLocale{
             self.font = newFont!;
         }
     }
+    public func bs_calculateMaxLines() -> Int {
+            let maxSize = CGSize(width: frame.size.width, height: CGFloat(Float.infinity))
+            let charSize = font.lineHeight
+            let text = (self.text ?? "") as NSString
+            let textSize = text.boundingRect(with: maxSize, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+            let linesRoundedUp = Int(ceil(textSize.height/charSize))
+            return linesRoundedUp
+        }
 }
 
 /*    **SystemSoundID**   */
