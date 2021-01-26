3d87160fea04753899fa2b53e0dbda0df65e55f6
Salah <Salahalimohamed1995@gmail.com>
2021-01-26 14:21:41 +0200

    no message

--- SalahUtilityExample/RegularExpressionViewController.swift
+++ SalahUtilityExample/RegularExpressionViewController.swift
@@ -7,23 +7,41 @@
 //
 
 import UIKit
-
+import SalahUtility
 class RegularExpressionViewController: UIViewController {
+    var emailRegix = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}";
+    var phoneNumberRegix = "[+]+[0-9]{1,}|[00]+[0-9]{1,}|[0-9]{9,}";
+    // explain '|' OR
+    // explain '[0-9]{9,}' OR 9 or more digits
+    // explain '[+]' string has prefix +
+    // explain '[00]+[0-9]{1,}' any number start with prefx 00 and contains one or more digits
+    // explain ']+['  + here used for build block
+
     @IBOutlet weak var btnTest: UIButton!
     
+    @IBOutlet weak var lblCotnent: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+
 
         // Do any additional setup after loading the view.
     }
     
     @IBAction func btnTest(_ sender: Any) {
         // for email
-        if Regex("[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}").test(input: "salah@sdad.com") {
-          print("succefull")
-        }else{
-            print("");
-        }
+//        if Regex(emailRegix).test(input: "salah@sdad.com") {
+//          print("succefull")
+//        }else{
+//            print("");
+//        }
+        
+       var  a = Regex(emailRegix).replacement(input:"dsadsadasdsdasdsadsadsadsadsad  salah@hotmail.com sal@ail.com", replacement:"x")
+        print(a);
+        self.lblCotnent.text=a;
+        
+        var  a2 = Regex(phoneNumberRegix).replacement(input:"dsadsadasdsdasdsadsadsadsadsad 12 22222222222323232323232323232 111111111 00972 +972597", replacement:"xxxxxxxxx")
+         print(a2);
+         self.lblCotnent.text=a2;
     }
     
     /*
@@ -38,20 +56,4 @@ class RegularExpressionViewController: UIViewController {
 
 }
 
-class Regex {
-  let internalExpression: NSRegularExpression
-  let pattern: String
 
-  init(_ pattern: String) {
-//    print(pattern);
-    self.pattern = pattern
-    var error: NSError?
-    self.internalExpression = try! NSRegularExpression(pattern: pattern, options: .caseInsensitive)
-    
-  }
-
-  func test(input: String) -> Bool {
-    let matches = self.internalExpression.matches(in:input, range:NSMakeRange(0,input.count))
-    return matches.count > 0
-  }
-}
