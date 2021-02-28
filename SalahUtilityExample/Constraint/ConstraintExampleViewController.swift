ef2cd8e49a32442ffe2cafbb92028170c00becd4
Salah <Salahalimohamed1995@gmail.com>
2021-02-28 13:06:07 +0200

    no message

--- SalahUtilityExample/Constraint/ConstraintExampleViewController.swift
+++ SalahUtilityExample/Constraint/ConstraintExampleViewController.swift
@@ -14,17 +14,50 @@ class ConstraintExampleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        var newView = UIView.init();
-        newView.backgroundColor=UIColor.red;
         var customeSuperView=self.view as! UIView
-        self.view.addSubview(newView);
-        newView.translatesAutoresizingMaskIntoConstraints = false
 
-        customeSuperView.addConstraint(NSLayoutConstraint(item: newView, attribute: .trailing, relatedBy: .equal, toItem: customeSuperView, attribute: .trailing, multiplier: 1, constant:10))
-        customeSuperView.addConstraint(NSLayoutConstraint(item: newView, attribute: .leading, relatedBy: .equal, toItem: customeSuperView, attribute: .leading, multiplier: 1, constant:10))
-        customeSuperView.addConstraint(NSLayoutConstraint(item: newView, attribute: .top, relatedBy: .equal, toItem: self.topLayoutGuide, attribute: .bottom, multiplier: 1, constant:10))
-        customeSuperView.addConstraint(NSLayoutConstraint(item: newView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute,multiplier: 1, constant: 131))
+//        // Do any additional setup after loading the view.
+        
+        /*
+        // used when space from traling and from leading and from top and from bottom
+        var newView1 = UIView.init();
+        newView1.translatesAutoresizingMaskIntoConstraints = false
+        newView1.backgroundColor=UIColor.red;
+        customeSuperView.addSubview(newView1);
+
+        customeSuperView.addConstraint(NSLayoutConstraint(item: newView1, attribute: .trailing, relatedBy: .equal, toItem: customeSuperView, attribute: .trailing, multiplier: 1, constant:-10))
+        customeSuperView.addConstraint(NSLayoutConstraint(item: newView1, attribute: .leading, relatedBy: .equal, toItem: customeSuperView, attribute: .leading, multiplier: 1, constant:10))
+        customeSuperView.addConstraint(NSLayoutConstraint(item: newView1, attribute: .top, relatedBy: .equal, toItem: self.topLayoutGuide, attribute: .bottom, multiplier: 1, constant:10))
+        customeSuperView.addConstraint(NSLayoutConstraint(item: newView1, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute,multiplier: 1, constant: 131))
+      */
+        
+        /*
+        // when add view equal another view
+        var newView2 = UIView();
+        newView2.translatesAutoresizingMaskIntoConstraints = false
+        newView2.backgroundColor=UIColor.blue
+        customeSuperView.addSubview(newView2);
+        NSLayoutConstraint.activate([
+            customeSuperView.topAnchor.constraint(equalTo: newView2.topAnchor),
+            customeSuperView.bottomAnchor.constraint(equalTo: newView2.bottomAnchor),
+            customeSuperView.leadingAnchor.constraint(equalTo: newView2.leadingAnchor),
+            customeSuperView.trailingAnchor.constraint(equalTo: newView2.trailingAnchor)
+        ])
+ */
+        
+        /*
+        // when add view equal another view with spacing
+        var newView3 = UIView();
+        newView3.translatesAutoresizingMaskIntoConstraints = false
+        newView3.backgroundColor=UIColor.orange
+        customeSuperView.addSubview(newView3);
+        NSLayoutConstraint.activate([
+            customeSuperView.topAnchor.constraint(equalTo: newView3.topAnchor,constant:-100),
+            customeSuperView.bottomAnchor.constraint(equalTo: newView3.bottomAnchor,constant:100),
+            customeSuperView.leadingAnchor.constraint(equalTo: newView3.leadingAnchor,constant:-100),
+            customeSuperView.trailingAnchor.constraint(equalTo: newView3.trailingAnchor,constant:100)
+        ])
+       */
     }
     
     @IBAction func btnAdd(_ sender: Any) {
