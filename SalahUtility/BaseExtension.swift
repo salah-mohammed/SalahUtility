8d46e0265fdc8525e441ad43ba0550eaaca08caa
Salah <Salahalimohamed1995@gmail.com>
2021-10-08 18:08:27 +0300

    no message

--- SalahUtility/BaseExtension.swift
+++ SalahUtility/BaseExtension.swift
@@ -838,6 +838,19 @@ public extension Double{
 }
 
  extension Array where Element == Int {
+    public func bs_bigestValue() -> Int?{
+        var array = self
+        var bigestValue:Int?
+        for item in array{
+            if let tempValue:Int = bigestValue,tempValue < item{
+                bigestValue = tempValue
+            }else{
+                bigestValue = item;
+            }
+        }
+        return bigestValue;
+    }
+    
     public func bs_sum() -> Int{
         var total = 0;
         for object in self {
