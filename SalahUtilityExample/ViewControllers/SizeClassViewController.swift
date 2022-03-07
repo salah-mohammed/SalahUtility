75fe188f1986fa9158522de4655f8c70277a8b90
Salah <Salahalimohamed1995@gmail.com>
2022-03-07 22:16:11 +0200

    no message

---
+++ SalahUtilityExample/ViewControllers/SizeClassViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SizeClassViewController.swift
+//  SalahUtilityExample
+//
+//  Created by Salah on 3/7/22.
+//  Copyright © 2022 Salah. All rights reserved.
+//
+
+import UIKit
+
+//  iphone 8+,7+ == iphone 12ProMax,iphone 11ProMax,iphone 13ProMax(ProMax)
+// Se 1G CR - portrait
+// Se 2G CR - portrait
+// Se 8  CR - portrait
+// 8+ CR - portrait
+// 12 mini  CR - portrait
+// 13   CR - portrait
+// 13 Pro   CR - portrait
+// 13 Pro Max   CR - portrait
+
+// Se 1G CC - landscape
+// Se 2G CC - landscape
+// Se 8 CC - landscape
+// 8+ RC - landscape
+// 12 mini  cc - landscape
+// 13   CC - landscape
+// 13 Pro   CC - landscape
+// 13 Pro Max   RC - landscape
+
+class SizeClassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
