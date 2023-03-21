9d7cfeed0fe49653511ce015b719b807aa2e1647
SalahMohamed <>
2023-03-21 11:20:51 +0200

    no message

---
+++ SalahUtility/BaseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.swift
+//  NamesOfChildren
+//
+//  Created by SalahMohamed on 21/03/2023.
+//
+
+import Foundation
+import UIKit
+open class BaseViewController:UIViewController {
+open var actions:[BaseAction]=[BaseAction]();
+
+}
