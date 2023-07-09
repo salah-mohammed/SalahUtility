d34fee8b571f7656357b6e69c994e72d414ef849
SalahMohamed <>
2023-07-09 15:25:53 +0300

    no message

--- SalahUtility/SwiftUIExtension.swift
+++ SalahUtility/SwiftUIExtension.swift
@@ -46,4 +46,32 @@ extension View {
              .overlay(roundedRect.strokeBorder(content, lineWidth: width))
     }
 }
+
+@available(iOS 13.0, *)
+public struct CornerRadiusStyle: ViewModifier {
+    var radius: CGFloat
+    var corners: UIRectCorner
+    
+    struct CornerRadiusShape: Shape {
+
+        var radius = CGFloat.infinity
+        var corners = UIRectCorner.allCorners
+
+        func path(in rect: CGRect) -> Path {
+            let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+            return Path(path.cgPath)
+        }
+    }
+
+    public func body(content: Content) -> some View {
+        content
+            .clipShape(CornerRadiusShape(radius: radius, corners: corners))
+    }
+}
+@available(iOS 13.0, *)
+public extension View {
+    func bs_cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        ModifiedContent(content: self, modifier: CornerRadiusStyle(radius: radius, corners: corners))
+    }
+}
 #endif
