a0ea1bacc31fa6f34fc908c78ced7b6b6827d9ca
SalahMohamed <>
2023-07-26 15:42:43 +0300

    fix

--- SalahUtility/SwiftUIExtension.swift
+++ SalahUtility/SwiftUIExtension.swift
@@ -55,6 +55,30 @@ public extension View {
             return self.foregroundColor(color)
         }
     }
+    /// Hide or show the view based on a boolean value.
+    ///
+    /// Example for visibility:
+    ///
+    ///     Text("Label")
+    ///         .isHidden(true)
+    ///
+    /// Example for complete removal:
+    ///
+    ///     Text("Label")
+    ///         .isHidden(true, remove: true)
+    ///
+    /// - Parameters:
+    ///   - hidden: Set to `false` to show the view. Set to `true` to hide the view.
+    ///   - remove: Boolean value indicating whether or not to remove the view.
+    @ViewBuilder func bs_isHidden(_ hidden: Bool, remove: Bool = false) -> some View {
+        if hidden {
+            if !remove {
+                self.hidden()
+            }
+        } else {
+            self
+        }
+    }
 }
 
 @available(iOS 13.0, *)
@@ -78,4 +102,5 @@ public struct CornerRadiusStyle: ViewModifier {
             .clipShape(CornerRadiusShape(radius: radius, corners: corners))
     }
 }
+
 #endif
