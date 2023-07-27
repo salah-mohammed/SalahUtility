8e3d96fdb94e1b4112608586157809bc63233e6f
SalahMohamed <>
2023-07-27 10:54:04 +0300

    fix

--- SalahUtility/SwiftUIExtension.swift
+++ SalahUtility/SwiftUIExtension.swift
@@ -55,29 +55,8 @@ public extension View {
             return self.foregroundColor(color)
         }
     }
-    /// Hide or show the view based on a boolean value.
-    ///
-    /// Example for visibility:
-    ///
-    ///     Text("Label")
-    ///         .isHidden(true)
-    ///
-    /// Example for complete removal:
-    ///
-    ///     Text("Label")
-    ///         .isHidden(true, remove: true)
-    ///
-    /// - Parameters:
-    ///   - hidden: Set to `false` to show the view. Set to `true` to hide the view.
-    ///   - remove: Boolean value indicating whether or not to remove the view.
-    @ViewBuilder func bs_isHidden(_ hidden: Bool, remove: Bool = false) -> some View {
-        if hidden {
-            if !remove {
-                self.hidden()
-            }
-        } else {
-            self
-        }
+    @ViewBuilder func bs_isHidden(value: Binding<Bool>) -> some View {
+            value.wrappedValue ? self  :  self.hidden() as? Self
     }
 }
 
