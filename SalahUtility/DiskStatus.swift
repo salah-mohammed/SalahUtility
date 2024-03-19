34a4bd463d27849bdfcc69fd90fc89cc8fe27513
SalahMohamed <>
2024-03-20 01:24:20 +0200

    no message

---
+++ SalahUtility/DiskStatus.swift
@@ -0,0 +1,74 @@
+//
+//  DiskStatus.swift
+//  SalatySalahy
+//
+//  Created by SalahMohamed on 19/03/2024.
+//  Copyright © 2024 Salah. All rights reserved.
+//
+public class DiskStatus {
+
+    //MARK: Formatter MB only
+    public class func GBFormatter(_ bytes: Int64) -> String {
+        let formatter = ByteCountFormatter()
+        formatter.allowedUnits = ByteCountFormatter.Units.useGB
+        formatter.countStyle = ByteCountFormatter.CountStyle.decimal
+        formatter.includesUnit = false
+        return (formatter.string(fromByteCount: bytes) as String)
+    }
+    public class func GBFormatterDouble(_ bytes: Int64) -> Double? {
+        return DiskStatus.GBFormatter(bytes).bs_replace(target:",", withString:"").bs_double
+    }
+
+    //MARK: Get String Value
+    public class var totalDiskSpace:String {
+        get {
+            return ByteCountFormatter.string(fromByteCount: totalDiskSpaceInBytes, countStyle: ByteCountFormatter.CountStyle.file)
+        }
+    }
+
+    public class var freeDiskSpace:String {
+        get {
+            return ByteCountFormatter.string(fromByteCount: freeDiskSpaceInBytes, countStyle: ByteCountFormatter.CountStyle.file)
+        }
+    }
+
+    public  class var usedDiskSpace:String {
+        get {
+            return ByteCountFormatter.string(fromByteCount: usedDiskSpaceInBytes, countStyle: ByteCountFormatter.CountStyle.file)
+        }
+    }
+
+
+    //MARK: Get raw value
+    public class var totalDiskSpaceInBytes:Int64 {
+        get {
+            do {
+                let systemAttributes = try FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory() as String)
+                let space = (systemAttributes[FileAttributeKey.systemSize] as? NSNumber)?.int64Value
+                return space!
+            } catch {
+                return 0
+            }
+        }
+    }
+
+    public class var freeDiskSpaceInBytes:Int64 {
+        get {
+            do {
+                let systemAttributes = try FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory() as String)
+                let freeSpace = (systemAttributes[FileAttributeKey.systemFreeSize] as? NSNumber)?.int64Value
+                return freeSpace!
+            } catch {
+                return 0
+            }
+        }
+    }
+
+    public class var usedDiskSpaceInBytes:Int64 {
+        get {
+            let usedSpace = totalDiskSpaceInBytes - freeDiskSpaceInBytes
+            return usedSpace
+        }
+    }
+
+}
