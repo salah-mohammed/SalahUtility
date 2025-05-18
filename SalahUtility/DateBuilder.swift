73a6d647ff5244c5c8fcde3c323fd9977216cab8
SalahMohamed <>
2025-05-18 14:14:10 +0300

    fix

--- SalahUtility/DateBuilder.swift
+++ SalahUtility/DateBuilder.swift
@@ -11,17 +11,23 @@ public typealias DateFormatType = String
 
 public protocol DateRequest{
     var calendar:Calendar{get}
-    var dateFormate:String{get set}
+    var dateFormatter:DateFormatter{get set}
     var local:Locale?{get}
     var timeZone:TimeZone?{get}
-    init( dateFormate: String)
+    init(stringDateFormate: String)
 }
 public class DateBuilder{
     var dateFormatter:DateFormatter?
     var dateRequest:DateRequest?
     var date:Date?
+    var dateComponents:(dateComponents:DateComponents,baseOnCalendar:Calendar)?
     public init() {
         
+    }
+    public typealias BaseDateComponents = (dateComponents:DateComponents,baseOnCalendar:Calendar)
+    public func dateComponents(dateComponents:BaseDateComponents)->Self{
+        self.dateComponents=dateComponents
+        return self
     }
     public func date(date:Date)->Self{
         self.date=date
@@ -40,11 +46,16 @@ public class DateBuilder{
     public func execute()->String?{
         if let date:Date = self.date{
             return self.dateFormatter?.string(from:date)
-        }else{return nil}
+        }else
+        if let dateComponents:(dateComponents:DateComponents,baseOnCalendar:Calendar) = self.dateComponents,
+           let date:Date = dateComponents.baseOnCalendar.date(from:dateComponents.dateComponents){
+            return self.dateFormatter?.string(from:date)
+        }else{
+            return nil
+        }
     }
     fileprivate func construct(request:DateRequest)->DateFormatter{
-     let dateFormatter = DateFormatter.init()
-        dateFormatter.dateFormat = request.dateFormate;
+        var dateFormatter = request.dateFormatter;
         dateFormatter.calendar = request.calendar
         dateFormatter.locale = request.local ?? Locale.current
         dateFormatter.timeZone = request.timeZone
